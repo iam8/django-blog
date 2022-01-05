1e99ae07b57879c8b6f8fae16a4550272cc72312
Ioana Alexandra Mititean <alex-m@outlook.com>
2022-01-04 18:09:30 -0800

    Defined detail view for blog posts; removed unecessary imports; removed stub view code

--- blogging/views.py
+++ blogging/views.py
@@ -1,23 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
+from django.http import Http404
 
 from blogging.models import Post
-
-
-
-def stub_view(request, *args, **kwargs):
-
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join([f"\t{a}" for a in args])
-
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join([f"\t{i}" for i in kwargs.items()])
-
-    return HttpResponse(body, content_type="text/plain")
 
 
 def list_view(request):
@@ -27,3 +11,16 @@ def list_view(request):
     context = {"posts": posts}
 
     return render(request, "blogging/list.html", context)
+
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+
+    context = {"post": post}
+
+    return render(request, "blogging/detail.html", context)
