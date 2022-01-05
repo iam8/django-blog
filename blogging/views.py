41d08240f9b58ff7682e2415616b4a23d0fa1099
Ioana Alexandra Mititean <alex-m@outlook.com>
2022-01-04 17:07:39 -0800

    Defined list view

--- blogging/views.py
+++ blogging/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+
+from blogging.models import Post
+
 
 
 def stub_view(request, *args, **kwargs):
@@ -14,3 +18,12 @@ def stub_view(request, *args, **kwargs):
         body += "\n".join([f"\t{i}" for i in kwargs.items()])
 
     return HttpResponse(body, content_type="text/plain")
+
+
+def list_view(request):
+
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by("-published_date")
+    context = {"posts": posts}
+
+    return render(request, "blogging/list.html", context)
