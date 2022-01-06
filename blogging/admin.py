8398e1eb7c2ee05521a4590e2bd68d786e49ff4c
Ioana Alexandra Mititean <alex-m@outlook.com>
2022-01-06 12:21:10 -0800

    Added ModelAdmin for Category and Post; added inline model for Category; registered the admin models.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.TabularInline):
+
+    model = Category.posts.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    exclude = ("posts",)
+
+
+class PostAdmin(admin.ModelAdmin):
+
+    inlines = [
+        CategoryInline,
+    ]
+
+
+# Registering the models
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
