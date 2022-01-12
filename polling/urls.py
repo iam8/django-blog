ff5e92b05ec9d925f2e307d0f0822755e3e32b11
Ioana Alexandra Mititean <alex-m@outlook.com>
2022-01-11 17:27:27 -0800

    Imported new PollDetailView and replaced usage of old detail_view with PollDetailView; replaced poll_id with 'pk'

--- polling/urls.py
+++ polling/urls.py
@@ -7,10 +7,10 @@ URL patterns for polling app.
 """
 
 from django.urls import path
-from polling.views import PollListView, detail_view
+from polling.views import PollListView, PollDetailView
 
 
 urlpatterns = [
     path("", PollListView.as_view(), name="poll_index"),
-    path("polls/<int:poll_id>/", detail_view, name="poll_detail")
+    path("polls/<int:pk>/", PollDetailView.as_view(), name="poll_detail")
 ]
