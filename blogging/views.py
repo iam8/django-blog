57f8471e8e0caa47931e23cbe34c4b04752c7bab
Ioana Alexandra Mititean <alex-m@outlook.com>
2022-01-12 12:06:42 -0800

    Commented out old function-based views; removed unused imports; removed TODOs

--- blogging/views.py
+++ blogging/views.py
@@ -6,8 +6,6 @@
 Code for the views for the blogging app.
 """
 
-from django.shortcuts import render
-from django.http import Http404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
@@ -22,8 +20,6 @@ class BlogListView(ListView):
     List view class for blogging app. Only published posts will be listed.
     """
 
-    # TODO: filter Posts so only published ones are shown; use QuerySets
-
     queryset = Post.objects.exclude(published_date__exact=None).order_by("-published_date")
     template_name = "blogging/list.html"
 
@@ -34,8 +30,6 @@ class BlogDetailView(DetailView):
     Detail view class for blogging app. Only published posts will have a detail view.
     """
 
-    # TODO: filter Posts so only published ones can have a detail view; use QuerySets
-
     queryset = Post.objects.exclude(published_date__exact=None)
     template_name = "blogging/detail.html"
 
@@ -44,34 +38,34 @@ class BlogDetailView(DetailView):
 
 # FUNCTION-BASED VIEWS (OLD) -----------------------------------------------------------------------
 
-def list_view(request):
-
-    """
-    List view of posts for the blogging app. Only published posts will be listed.
-    """
-
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by("-published_date")
-    context = {"posts": posts}
-
-    return render(request, "blogging/list.html", context)
-
-
-def detail_view(request, post_id):
-
-    """
-    Detail view of posts for the blogging app. Only published posts will have this view.
-    """
-
-    published = Post.objects.exclude(published_date__exact=None)
-
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist as err:
-        raise Http404 from err
-
-    context = {"post": post}
-
-    return render(request, "blogging/detail.html", context)
+#def list_view(request):
+#
+#    """
+#    List view of posts for the blogging app. Only published posts will be listed.
+#    """
+#
+#    published = Post.objects.exclude(published_date__exact=None)
+#    posts = published.order_by("-published_date")
+#    context = {"posts": posts}
+#
+#    return render(request, "blogging/list.html", context)
+#
+#
+#def detail_view(request, post_id):
+#
+#    """
+#    Detail view of posts for the blogging app. Only published posts will have this view.
+#    """
+#
+#    published = Post.objects.exclude(published_date__exact=None)
+#
+#    try:
+#        post = published.get(pk=post_id)
+#    except Post.DoesNotExist as err:
+#        raise Http404 from err
+#
+#    context = {"post": post}
+#
+#    return render(request, "blogging/detail.html", context)
 
 #---------------------------------------------------------------------------------------------------
