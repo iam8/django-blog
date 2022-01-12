57db40807cd3682ee53f2461af8b3d29e95dfa09
Ioana Alexandra Mititean <alex-m@outlook.com>
2022-01-11 16:56:56 -0800

    Readded import and use of detail_view function for now

--- polling/urls.py
+++ polling/urls.py
@@ -7,10 +7,10 @@ URL patterns for polling app.
 """
 
 from django.urls import path
-from polling.views import PollListView
+from polling.views import PollListView, detail_view
 
 
 urlpatterns = [
     path("", PollListView().as_view(), name="poll_index"),
-    #path("polls/<int:poll_id>/", detail_view, name="poll_detail")
+    path("polls/<int:poll_id>/", detail_view, name="poll_detail")
 ]
