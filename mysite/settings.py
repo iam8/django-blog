b47f1ee8babcfdb49313c372bdf7d3a3531768eb
Alex M <alex-m@outlook.com>
2022-01-21 11:49:44 -0800

    Applied black

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,12 +44,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sites",
-
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
     "allauth.socialaccount.providers.github",
-
     "polling",
     "blogging",
 ]
