efd07bab92ef878e7d5aeefef8b8a3f37b75bfda
Alex M <alex-m@outlook.com>
2022-01-24 21:53:13 -0800

    Applied black

--- mysite/urls.py
+++ mysite/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("", include("blogging.urls")),
     path("polling/", include("polling.urls")),
-    #path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    # path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
 ]
